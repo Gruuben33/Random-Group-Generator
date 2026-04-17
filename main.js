1e64bc771f52f4d828b8fab784d6c2ce1625a15c
Gruuben33 <ruubenpuddle@gmail.com>
2026-04-17 13:35:05 -0400

    Completed Group Generator

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
     background(220);
+    textSize(50);
 
-    n = [Jesse, Jack, James, John, Jerry, Jacob, Justin, Jordan];
-    m = 4;
+    n = ['Pesse', 'Gack', 'Sames', 'Tohn', 'Verry', 'Qacob', 'Nustin', 'Wordan'];
+    m = 3;
     groups = {};
-    grouping = generateGroups(n, m);
-    text(`${grouping}`, windowWidth/2, windowHeight/2)
+    generateGroups(n, m);
+    // grouping = generateGroups(n, m);
+    // [key, names] = Object.entries(grouping);
+    // for (let i = 0; i < Object.entries(grouping)[0].length; i++) {
+        // text(`Group ${i + 1}: ${names[i]}`, 50, 50)
+    // text(`${grouping}`, 50, 50)
+    // }
 }
 
 function generateGroups(n, m) {
     let numberOfGroups = Math.ceil(n.length / m);
     for (let j = 0; j < numberOfGroups; j++) {
-            groups[i] = [];
+            text(`Group ${j + 1}:`, windowWidth/2 - 250, windowHeight/2 + j * 100);
+            groups[j] = [];
         for (let i = 0; i < m; i++) {
-            let indexOfPerson = random(0, n.length - 1)
+            let indexOfPerson = Math.round(random(0, n.length - 1))
             let person = n[indexOfPerson];
             groups[j].push(person);
+            text(`${groups[j]}`, windowWidth/2 + 100, windowHeight/2 + j * 100);
             n.splice(indexOfPerson, 1);
         }
     }
